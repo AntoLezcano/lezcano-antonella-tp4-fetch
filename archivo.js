e27b17ac838761f7a5781f27b7f36ab75c070a61
Antonella <antonella.beti@gmail.com>
2024-06-05 11:52:09 -0300

    se trabajo en javaScript y html

---
+++ archivo.js
@@ -0,0 +1,29 @@
+
+
+const contenido = document.getElementById("contenidoPok")
+const fetchData = async() =>{
+    try{
+        const res = await fetch("https://rickandmortyapi.com/api/character");
+        const  data = await res.json()
+        const pregunta = data.results
+        
+    pregunta.forEach(pregunt => {
+            const item = document.createElement("div")
+            item.className ="cartita" 
+            item.innerHTML += `
+            
+            <img src="${pregunt.image}" alt="">
+            <h3>${pregunt.name} </h3>
+            <p> ${pregunt.status} </p>
+            <p> ${pregunt.species} </p>
+            <p> ${pregunt.episode} </p>
+            `
+            contenido.appendChild(item)
+
+
+        });
+    }catch(error){
+        console.log("Se produjo un error: ",)
+    }
+}
+fetchData()
